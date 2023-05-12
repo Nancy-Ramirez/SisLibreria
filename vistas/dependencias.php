9c425d1f11bb0542aef98f238e301fb48f38d428
JennyMonge <MM16110@ues.edu.sv>
2023-05-12 01:42:22 -0600

    dashboard

--- vistas/dependencias.php
+++ vistas/dependencias.php
@@ -6,8 +6,8 @@
 <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
 <link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="../librerias/select2/css/select2.css">
-
 <link rel="stylesheet" type="text/css" href="../css/card.css">
+
 
 <script src="../librerias/jquery-3.2.1.min.js"></script>
 <script src="../librerias/alertifyjs/alertify.js"></script>
--- vistas/menu.php
+++ vistas/menu.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+     <!-- Begin Navbar -->
     <nav class="navbar navbar-expand-lg  navbar-fixed-top" style="background-color: #023535;">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img class="" src="../img/logo1.png" alt="" width="50px" height="50px"></a>
@@ -82,134 +83,8 @@
             </div>
         </div>
     </nav>
-    <!-- Begin Navbar -->
-    <div id="nav">
-        <div class="navbar navbar-fixed-top" data-spy="affix" data-offset-top="100" style="background-color: #023535;">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="inicio.php"><img class="" src="../img/logo1.png" alt="" width="75px" height="75px"></a>
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                </div>
-                <div id="navbar" class="collapse navbar-collapse">
-
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <!--Inicio-->
-                        <li>
-                            <a href="inicio.php" style="color: white">
-                                <span class="glyphicon glyphicon-home"></span>
-                                Inicio
-                            </a>
-                        </li>
-                        <!--Ventas-->
-                        <li><a href="ventas.php" style="color: white"><span class="glyphicon glyphicon-usd"></span>
-                                Ventas</a>
-                        </li>
-                        <!--Compras-->
-                        <?php
-                        if ($_SESSION['usuario'] == 'Administrador') :
-                        ?>
-                            <li><a href="compras.php?pagina=1" style="color: white"><span class="glyphicon glyphicon-usd"></span>
-                                    Compras</a>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-
-                        <!--Articulos-->
-                        <?php
-                        if ($_SESSION['usuario'] == "Jefe") :
-                        ?>
-                            <li><a href="articulos.php?pagina=1" style="color: white"><span class="glyphicon glyphicon-apple"></span>
-                                    Articulo</a>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-                        <?php
-                        if ($_SESSION['usuario'] == "Administrador") :
-                        ?>
-                            <li><a href="articulos.php?pagina=1" style="color: white"><span class="glyphicon glyphicon-apple"></span>
-                                    Articulo</a>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-
-                        <!--Comisión-->
-                        <?php
-                        if ($_SESSION['usuario'] == "Jefe") :
-                        ?>
-                            <li class="dropdown">
-                                <a href="#" style="color: white" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span> Comisión<span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="comision.php">Reglas comisión</a></li>
-                                    <li><a href="#">Reporte comision</a></li>
-                                </ul>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-                        <?php
-                        if ($_SESSION['usuario'] == "Administrador") :
-                        ?>
-                            <li class="dropdown">
-                                <a href="#" style="color: white" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span> Comisión<span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="comision.php">Reglas comisión</a></li>
-                                    <li><a href="#">Reporte comision</a></li>
-                                </ul>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-
-                        <!--Personal-->
-                        <?php
-                        if ($_SESSION['usuario'] == "Administrador") :
-                        ?>
-                            <li class="dropdown">
-                                <a href="#" style="color: white" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="	glyphicon glyphicon-edit"></span> Personal <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="empleados.php?pagina=1">Empleados</a></li>
-                                    <li><a href="departamento.php">Departamento</a></li>
-                                </ul>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-                        <!--Usuarios-->
-                        <?php
-                        if ($_SESSION['usuario'] == "Administrador") :
-                        ?>
-                            <li><a href="usuarios.php" style="color: white"><span class="glyphicon glyphicon-user"></span>
-                                    Usuarios</a>
-                            </li>
-                        <?php
-                        endif;
-                        ?>
-
-                        <li class="dropdown">
-                            <a href="#" style="color: #41a0b0" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-off"></span>
-                                <?php echo $_SESSION['usuario']; ?> <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li> <a style="color: white" href="../procesos/salir.php"><span class="glyphicon glyphicon-off"></span> Salir</a></li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <!--/.nav-collapse -->
-            </div>
-            <!--/.contatiner -->
-        </div>
-    </div>
+   
+   
     <!-- Main jumbotron for a primary marketing message or call to action -->
 
 
